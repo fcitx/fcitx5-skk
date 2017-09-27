ec7e0e480c4717b73f3c31c0a88b5c412aaab86f
Jiachen Yang <farseerfc@gmail.com>
2017-09-28 00:45:06 +0900

    writing ctor

--- src/skk.cpp
+++ src/skk.cpp
@@ -1,8 +1,27 @@
 #include "skk.hpp"
-
 namespace fcitx {
-    SkkEngine::SkkEngine(Instance *instance):instance_{instance} {
-        // FIXME
+    SkkEngine::SkkEngine(Instance *instance):
+        instance_{instance},
+        factory_([this](InputContext &) { return new SkkState(this); }) 
+    {
+        skk_context_set_period_style(context_, SKK_PERIOD_STYLE_JA_JA);
+        skk_context_set_input_mode(context_, SKK_INPUT_MODE_HIRAGANA);
+
+        instance_ -> inputContextManager().registerProperty("skkState", &factory_);
+    
+        wchar_t* AUTO_START_HENKAN_KEYWORDS[] = {
+            "を", "、", "。", "．", "，", "？", "」",
+            "！", "；", "：", ")", ";", ":", "）",
+            "”", "】", "』", "》", "〉", "｝", "］",
+            "〕", "}", "]", "?", ".", ",", "!"
+        };
+        
+        skk_context_set_auto_start_henkan_keywords(context_, 
+                                                   AUTO_START_HENKAN_KEYWORDS,        
+                                                   G_N_ELEMENTS(AUTO_START_HENKAN_KEYWORDS));
+
+    
+
     }
     
     void SkkEngine::activate(const InputMethodEntry &entry, InputContextEvent &event){
--- src/skk.hpp
+++ src/skk.hpp
@@ -4,12 +4,18 @@
 #include <fcitx/addonfactory.h>
 #include <fcitx/instance.h>
 #include <fcitx/addonmanager.h>
+#include <fcitx/inputcontextproperty.h>
+#include <fcitx/inputcontextmanager.h>
+
+#include <libskk/libskk.h>
 
 namespace fcitx  {
+
+class SkkState;
     
 class SkkEngine final: public InputMethodEngine {
 public:
-    SkkEngine(Instance *instance);
+    SkkEngine(Instance *instance);s
     ~SkkEngine();
     
     void activate(const InputMethodEntry &entry, InputContextEvent &event) override;
@@ -17,9 +23,14 @@ public:
     void reloadConfig() override;
     void reset(const InputMethodEntry &entry, InputContextEvent &event) override;
     void save() override;
-
+    
+    auto &factory() { return factory_; }
+    SkkContext* context() { return context_; }
+    
 private:
     Instance *instance_;
+    FactoryFor<SkkState> factory_;
+    unique_ptr<SkkContext> context_;
 };
 
 class SkkAddonFactory final: public AddonFactory {
@@ -28,5 +39,13 @@ public:
         return new SkkEngine(manager->instance());
     }
 };
+
+
+class SkkState final: public InputContextProperty {
+    SkkState(SkkEngine* engine) : context_{engine -> context} {}
+private:
+    SkkContext *context_;
+};
+
 
 }
