ce4e0f9f588cf94e181b8a9c21e130d899e4c781
Jiachen Yang <farseerfc@gmail.com>
2017-10-28 04:51:19 +0900

    completing SkkState ctor

--- src/skk.cpp
+++ src/skk.cpp
@@ -61,21 +61,21 @@ namespace fcitx {
         
         UpdateInputMode();
         candidate_selected_handler = g_signal_connect(skk_context_get_candidates(context), "selected", G_CALLBACK(candidate_list_selected_cb), this);
-        //auto candidate_populated_handler = g_signal_connect(skk_context_get_candidates(context), "populated", G_CALLBACK(skk_candidate_list_popuplated_cb), this);
-        //auto notify_preedit_handler = g_signal_connect(context, "notify::preedit", G_CALLBACK(skk_candidate_update_preedit_cb), this);
-        //auto retrieve_surrounding_text_handler = g_signal_connect(context, "retrieve_surrounding_text", G_CALLBACK(skk_context_retrieve_surrounding_text_cb), this);
-        //auto delete_surrounding_text_handler = g_signal_connect(context, "delete_surrounding_text", G_CALLBACK(skk_context_delete_surrounding_text_cb), this);
+        candidate_list_populated_handler = g_signal_connect(skk_context_get_candidates(context), "populated", G_CALLBACK(candidate_list_popuplated_cb), this);
+        notify_preedit_handler = g_signal_connect(context, "notify::preedit", G_CALLBACK(notify_preedit_cb), this);
+        retrieve_surrounding_text_handler = g_signal_connect(context, "retrieve_surrounding_text", G_CALLBACK(retrieve_surrounding_text_cb), this);
+        delete_surrounding_text_handler = g_signal_connect(context, "delete_surrounding_text", G_CALLBACK(delete_surrounding_text_cb), this);
     
-        //gchar* AUTO_START_HENKAN_KEYWORDS[] = {
-        //    "を", "、", "。", "．", "，", "？", "」",
-        //    "！", "；", "：", ")", ";", ":", "）",
-        //    "”", "】", "』", "》", "〉", "｝", "］",
-        //    "〕", "}", "]", "?", ".", ",", "!"
-        //};
-        //
-        //skk_context_set_auto_start_henkan_keywords(context, 
-        //                                           AUTO_START_HENKAN_KEYWORDS,        
-        //                                           G_N_ELEMENTS(AUTO_START_HENKAN_KEYWORDS));
+        const char* AUTO_START_HENKAN_KEYWORDS[] = {
+            "を", "、", "。", "．", "，", "？", "」",
+            "！", "；", "：", ")", ";", ":", "）",
+            "”", "】", "』", "》", "〉", "｝", "］",
+            "〕", "}", "]", "?", ".", ",", "!"
+        };
+        
+        skk_context_set_auto_start_henkan_keywords(context, 
+                                                   const_cast<gchar**>(AUTO_START_HENKAN_KEYWORDS),        
+                                                   G_N_ELEMENTS(AUTO_START_HENKAN_KEYWORDS));
     }
     
     
@@ -86,6 +86,7 @@ namespace fcitx {
     void SkkState::input_mode_changed_cb(GObject *, GParamSpec *, SkkState *skk){
         skk -> UpdateInputMode();
     }
+    
     void SkkState::candidate_list_selected_cb(SkkCandidateList *, GParamSpec *, SkkState *skk){
         skk->selected = true;
         SkkContext * context = skk->context_.get();
@@ -93,10 +94,36 @@ namespace fcitx {
 
         if (output && strlen(output) > 0) {
             skk -> ic_ -> commitString(output);
-            //FcitxInstanceCommitString(skk->owner, FcitxInstanceGetCurrentIC(skk->owner), output);
         }
 
         g_free(output);
+    }
+    
+    void SkkState::candidate_list_popuplated_cb(SkkCandidateList *, GParamSpec *, SkkState *skk){
+        skk->updateCandidate = true;
+    }
+    
+    void SkkState::notify_preedit_cb(GObject *, GParamSpec *, SkkState *skk){
+        skk->updatePreedit = true;
+    }
+
+    gboolean SkkState::retrieve_surrounding_text_cb(GObject *, gchar** text, guint* cursor_pos, SkkState *skk){
+        InputContext* ic = skk->ic_;
+        if (!(ic->capabilityFlags().test(CapabilityFlag::SurroundingText)) || !ic->surroundingText().isValid())
+            return false;
+
+        *text = g_strdup(ic->surroundingText().selectedText().c_str());
+        *cursor_pos = ic->surroundingText().cursor();
+
+        return true;
+    }
+    
+    gboolean SkkState::delete_surrounding_text_cb(GObject *, gint offset, guint nchars, SkkState *skk){
+        InputContext* ic = skk->ic_;
+        if (!(ic->capabilityFlags().test(CapabilityFlag::SurroundingText)))
+            return false;
+        ic->deleteSurroundingText(offset, nchars);
+        return true;
     }
 }
 
--- src/skk.hpp
+++ src/skk.hpp
@@ -8,6 +8,7 @@
 #include <fcitx/addonmanager.h>
 #include <fcitx/inputcontextproperty.h>
 #include <fcitx/inputcontextmanager.h>
+#include <fcitx-utils/capabilityflags.h>
 
 #include <libskk/libskk.h>
 
@@ -43,14 +44,16 @@ public:
 
 class SkkState final: public InputContextProperty {
 public: 
-    SkkState(SkkEngine* engine, InputContext * ic);
+    SkkState(SkkEngine* engine, InputContext* ic);
 private:
     SkkEngine *engine_;
-    InputContext * ic_;
+    InputContext* ic_;
     std::unique_ptr<SkkContext, decltype(&g_object_unref)> context_;
 
     
     bool selected;
+    bool updateCandidate;
+    bool updatePreedit;
     
     void UpdateInputMode();
     
@@ -59,6 +62,14 @@ private:
     static void input_mode_changed_cb(GObject * gobject, GParamSpec *pspec, SkkState * skk);
     gulong candidate_selected_handler;
     static void candidate_list_selected_cb(SkkCandidateList * gobject, GParamSpec *pspec, SkkState *skk);
+    gulong candidate_list_populated_handler;
+    static void candidate_list_popuplated_cb(SkkCandidateList * gobject, GParamSpec *pspec, SkkState *skk);    
+    gulong notify_preedit_handler;
+    static void notify_preedit_cb(GObject * gobject, GParamSpec *pspec, SkkState *skk); 
+    gulong retrieve_surrounding_text_handler;
+    static gboolean retrieve_surrounding_text_cb(GObject *, gchar** text, guint* cursor_pos, SkkState *skk); 
+    gulong delete_surrounding_text_handler;
+    static gboolean delete_surrounding_text_cb(GObject *, gint offset, guint nchars, SkkState *skk); 
     
 };
 
