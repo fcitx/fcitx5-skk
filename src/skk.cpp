3a7c69baec00bbd32b0932c5674afac42034af90
YOSHIOKA Takuma <nop_thread@nops.red>
2025-04-30 23:54:02 +0900

    Update input panel on preedit clearing (#18)

--- src/skk.cpp
+++ src/skk.cpp
@@ -754,6 +754,10 @@ void SkkState::updateUI() {
         inputPanel.reset();
         ic_->updatePreedit();
         engine_->instance()->showInputMethodInformation(ic_);
+        if (!lastIsEmpty) {
+            // Preedit was cleared.
+            ic_->updateUserInterface(UserInterfaceComponent::InputPanel);
+        }
         return;
     }
 
